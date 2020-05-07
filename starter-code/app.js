5a8c32c5fc215a7c9df0eb041dbeea0055af56d3
Artur Serra <artur.aserra@gmail.com>
2020-05-07 09:39:26 +0100

    Lab Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,4 +16,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beers', (req, res) => {
+    punkAPI
+        .getBeers()
+        .then((beersApi) => {
+            // this will create the keys that will be accessed in the beers.hbs
+            res.render('beers', { beers: beersApi });
+        })
+        .catch(error => console.log(error))
+});
+
+app.get('/random-beer', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then((randomBeerFromApi) => {
+            res.render('random_beer', {randomBeer: randomBeerFromApi[0] });
+        })
+        .catch(error => console.log(error));
+})
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
